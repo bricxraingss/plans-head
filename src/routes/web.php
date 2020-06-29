4c4fbccc5b5dc85c13ab31e37ae4cdfeb29cb3ed
Marvin Mendiola <marvinmendiola.gss@gmail.com>
2020-06-29 16:14:11 +0800

    added features for admin and user

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,4 +11,18 @@
 |
 */
 
-Route::get('/', 'WelcomeController@welcome');
+Route::prefix('/')->group(function(){
+  Route::get('create', 'User\UsersController@create')->name('user.create');
+  Route::post('store', 'User\UsersController@store')->name('user.store');
+  Route::get('login', 'User\UsersController@login')->middleware('guest')->name('user.login');
+  Route::post('verify', 'User\UsersController@verify')->name('user.auth');
+  Route::get('logout', 'User\UsersController@logout')->name('user.logout');
+  
+  Route::middleware('admin')->prefix('admin')->group(function(){
+    Route::get('dashboard', 'Admin\AdminsController@index')->name('admin.dashboard');
+  });
+  
+  Route::middleware('user')->group(function(){
+    Route::get('dashboard', 'User\UsersController@index')->name('user.dashboard');
+  });
+});
